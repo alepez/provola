8569186a9af033e4c7d09231b59800ae75863dee
Alessandro Pezzato <alepez@users.noreply.github.com>
2021-11-29 20:29:17 +0100

    Refactoring

--- provola-core/src/build/mod.rs
+++ provola-core/src/build/mod.rs
@@ -1,6 +1,6 @@
+mod rust;
+
 use std::path::PathBuf;
-use std::process::Command;
-use std::process::Output;
 
 use crate::actions::Source;
 use crate::errors::Error;
@@ -8,30 +8,7 @@ use crate::lang::Language;
 
 pub fn gen_executable(lang: Language, source: &Source) -> Result<PathBuf, Error> {
     match lang {
-        Language::Rust => build_rust(source),
+        Language::Rust => rust::build(source),
         _ => todo!(),
-    }
-}
-
-fn make_build_rust_command(exec: &PathBuf, source: &Source) -> Command {
-    let mut cmd = Command::new("rustc");
-    cmd.arg(&source.0).arg("-o").arg(exec);
-    cmd
-}
-
-fn build_rust(source: &Source) -> Result<PathBuf, Error> {
-    let exec = PathBuf::from("./tmp.exe");
-    let mut cmd = make_build_rust_command(&exec, source);
-    log::info!("Running {:?}", cmd);
-    let Output { status, stderr, .. } = cmd.output()?;
-
-    let stderr = String::from_utf8(stderr).unwrap_or_default();
-
-    if status.success() {
-        Ok(exec)
-    } else {
-        let msg = format!("Cannot build executable with command {:?}: {}", cmd, stderr);
-        log::error!("{}", &msg);
-        Err(Error::BuildFailed(msg))
     }
 }
---
+++ provola-core/src/build/rust.rs
@@ -0,0 +1,29 @@
+use std::path::PathBuf;
+use std::process::Command;
+use std::process::Output;
+
+use crate::actions::Source;
+use crate::errors::Error;
+
+fn gen_command(exec: &PathBuf, source: &Source) -> Command {
+    let mut cmd = Command::new("rustc");
+    cmd.arg(&source.0).arg("-o").arg(exec);
+    cmd
+}
+
+pub(crate) fn build(source: &Source) -> Result<PathBuf, Error> {
+    let exec = PathBuf::from("./tmp.exe");
+    let mut cmd = gen_command(&exec, source);
+    log::info!("Running {:?}", cmd);
+    let Output { status, stderr, .. } = cmd.output()?;
+
+    let stderr = String::from_utf8(stderr).unwrap_or_default();
+
+    if status.success() {
+        Ok(exec)
+    } else {
+        let msg = format!("Cannot build executable with command {:?}: {}", cmd, stderr);
+        log::error!("{}", &msg);
+        Err(Error::BuildFailed(msg))
+    }
+}
