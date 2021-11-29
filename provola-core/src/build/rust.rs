4d2e468f2c8bc8fa5603baf110c9ba26089ff708
Alessandro Pezzato <alepez@users.noreply.github.com>
2021-11-29 22:41:04 +0100

    Add tests

--- provola-core/src/build/rust.rs
+++ provola-core/src/build/rust.rs
@@ -13,3 +13,35 @@ fn gen_command(exec: &PathBuf, source: &Source) -> Command {
 pub(crate) fn build(source: &Source) -> Result<PathBuf, Error> {
     super::common::build(source, gen_command)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn gen_source(s: &str) -> Source {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push(s);
+        Source::new(path)
+    }
+
+    #[test]
+    fn build_valid_program() {
+        let source = gen_source("examples/data/app_to_be_tested.rs");
+        let exec = build(&source);
+        assert!(exec.is_ok());
+    }
+
+    #[test]
+    fn build_non_existent_program() {
+        let source = gen_source("examples/data/this_file_does_not_exist.rs");
+        let exec = build(&source);
+        assert!(exec.is_err());
+    }
+
+    #[test]
+    fn build_invalid_program() {
+        let source = gen_source("examples/data/invalid_program.rs");
+        let exec = build(&source);
+        assert!(exec.is_err());
+    }
+}
