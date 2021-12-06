cd436b11eb9f286f46eb5c72ff35adf1fd1a93fa
Alessandro Pezzato <alepez@users.noreply.github.com>
2021-12-06 23:00:08 +0100

    Improve core Report

--- provola-core/src/report.rs
+++ provola-core/src/report.rs
@@ -1,47 +1,56 @@
 use serde::{Deserialize, Serialize};
 
-type Timestamp = String; // TODO
-type Duration = String; // TODO
-type Size = usize;
+type Timestamp = std::time::SystemTime;
+type Duration = std::time::Duration;
+type Count = usize;
+type Name = String;
+type Status = String;
+type Hostname = String;
+type Id = String;
+type Package = String;
+type ClassName = String;
 
-struct Report {
-    disabled: Option<Size>,
-    errors: Option<Size>,
-    failures: Option<Size>,
-    name: Option<String>,
-    tests: Option<Size>,
-    testsuites: Vec<TestSuite>,
-    time: Option<Duration>,
-    timestamp: Option<Timestamp>,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Report {
+    pub disabled: Option<Count>,
+    pub errors: Option<Count>,
+    pub failures: Option<Count>,
+    pub name: Option<Name>,
+    pub tests: Option<Count>,
+    pub testsuites: Vec<TestSuite>,
+    pub time: Option<Duration>,
+    pub timestamp: Option<Timestamp>,
 }
 
-struct TestSuite {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TestSuite {
     // TODO properties
     // TODO system-err
     // TODO system-out
-    disabled: Option<Size>,
-    errors: Option<Size>,
-    failures: Option<Size>,
-    hostname: Option<String>,
-    id: Option<String>,
-    name: String,
-    package: Option<String>,
-    skipped: Option<Size>,
-    testcases: Vec<TestCase>,
-    tests: Size,
-    time: Option<Duration>,
-    timestamp: Option<Time>,
+    pub disabled: Option<Count>,
+    pub errors: Option<Count>,
+    pub failures: Option<Count>,
+    pub hostname: Option<Hostname>,
+    pub id: Option<Id>,
+    pub name: Name,
+    pub package: Option<Package>,
+    pub skipped: Option<Count>,
+    pub testcases: Vec<TestCase>,
+    pub tests: Count,
+    pub time: Option<Duration>,
+    pub timestamp: Option<Duration>,
 }
 
-struct TestCase {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TestCase {
     // TODO error
+    // TODO assertions
     // TODO failure
     // TODO skipped
     // TODO system-err
     // TODO system-out
-    assertions: Option<String>,
-    classname: Option<String>,
-    name: String,
-    status: Option<String>,
-    time: Option<Duration>,
+    pub classname: Option<ClassName>,
+    pub name: Name,
+    pub status: Option<Status>,
+    pub time: Option<Duration>,
 }
