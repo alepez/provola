02ec7df0c84fdfdf2ce590adabbed62f9fce9416
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2021-12-11 00:31:36 +0100

    Make googletest an optional feature

--- provola-testrunners/src/lib.rs
+++ provola-testrunners/src/lib.rs
@@ -1,7 +1,10 @@
 use provola_core::test_runners::{TestRunner, TestRunnerInfo, TestRunnerType};
 
 pub fn make_test_runner(info: TestRunnerInfo) -> Box<dyn TestRunner> {
-    match info.trt {
+    let test_runner_type = info.trt;
+    match test_runner_type {
+        #[cfg(feature = "googletest")]
         TestRunnerType::GoogleTest => provola_googletest::TestRunner::from_executable(info.exec),
+        _ => todo!()
     }
 }
