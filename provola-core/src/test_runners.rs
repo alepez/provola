a122e2a33137e08f4df331dc899339041d06f6c0
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2021-12-20 22:44:58 +0100

    Add TestRunner::list

--- provola-core/src/test_runners.rs
+++ provola-core/src/test_runners.rs
@@ -5,9 +5,27 @@ pub trait TestRunner {
     fn run(&self) -> Result<TestResult, Error> {
         Err(Error::TestRunnerFeatureNotAvailable(TestRunnerFeature::Run))
     }
+
+    fn list(&self) -> Result<TestList, Error> {
+        Err(Error::TestRunnerFeatureNotAvailable(
+            TestRunnerFeature::List,
+        ))
+    }
 }
 
 #[derive(Debug, Display)]
 pub enum TestRunnerFeature {
     Run,
+    List,
 }
+
+struct TestSuite(String);
+
+struct TestCase(String);
+
+struct FullyQualifiedTestCase {
+    test_suite: TestSuite,
+    test_case: TestCase,
+}
+
+pub struct TestList(Vec<FullyQualifiedTestCase>);
