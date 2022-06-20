b95025d3a55bb2f9135cdd5a2e4164bfd0b082e6
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-06-20 23:47:11 +0200

    impl Display for CodeReference

--- provola_core/src/code.rs
+++ provola_core/src/code.rs
@@ -1,9 +1,16 @@
+use std::fmt::{Display, Formatter};
 use std::path::PathBuf;
 
 #[derive(Debug, Clone)]
 pub struct CodeReference {
     pub path: PathBuf,
     pub line: usize,
+}
+
+impl Display for CodeReference {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}", self.path.as_os_str().to_str().unwrap(), self.line)
+    }
 }
 
 #[cfg(test)]
@@ -17,5 +24,14 @@ mod tests {
             line: 32,
         };
         assert_eq!(32, cr.line)
+    }
+
+    #[test]
+    fn code_reference_can_be_displayed() {
+        let cr = CodeReference {
+            path: PathBuf::from("/foo/bar.rs"),
+            line: 32,
+        };
+        assert_eq!("/foo/bar.rs:32", cr.to_string())
     }
 }
