9f27b24b85f00b2f3be8c846d372921a847517d9
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-11-09 00:22:24 +0100

    Remove test_suite.rs

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -3,11 +3,9 @@ mod error;
 mod named;
 mod report;
 mod test_case;
-mod test_suite;
 mod testable;
 
 pub use named::Named;
 pub use report::FailDetails;
 pub use test_case::TestCase;
-pub use test_suite::TestSuite;
 pub use testable::Testable;
