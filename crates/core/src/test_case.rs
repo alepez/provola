4803ec5a2816db6af107c67e3219526c8d1adfbc
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-11-09 00:27:50 +0100

    Clean up

--- crates/core/src/test_case.rs
+++ crates/core/src/test_case.rs
@@ -1,35 +1,8 @@
-use crate::named::Named;
-use crate::report::PendingReport;
 use crate::testable::Testable;
 
-pub struct TestCase {
-    name: String,
-    runner: Box<dyn Testable>,
-    ignored: bool,
+pub trait Ignorable {
+    fn is_ignored(&self) -> bool;
+    fn set_ignored(&self) -> Result<(), ()>;
 }
 
-impl Testable for TestCase {
-    fn start(&self) -> Box<dyn PendingReport> {
-        self.runner.start()
-    }
-
-    fn is_ignored(&self) -> bool {
-        self.ignored
-    }
-}
-
-impl Named for TestCase {
-    fn name(&self) -> &str {
-        &self.name
-    }
-}
-
-impl TestCase {
-    pub fn new(name: impl Into<String>, runner: Box<dyn Testable>) -> Self {
-        Self {
-            name: name.into(),
-            runner,
-            ignored: false,
-        }
-    }
-}
+pub trait TestCase: Ignorable + Testable {}
