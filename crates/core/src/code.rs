95fba51534d06848ab9287883dde0527bd36245d
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-11-08 22:23:03 +0100

    Refactor CodeReference

--- crates/core/src/code.rs
+++ crates/core/src/code.rs
@@ -1,15 +1,38 @@
 use std::fmt::{Display, Formatter};
 use std::path::PathBuf;
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct CodeLine(u32);
+
+impl From<u32> for CodeLine {
+    fn from(n: u32) -> Self {
+        Self(n)
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct CodeReference {
-    pub path: PathBuf,
-    pub line: usize,
+    path: PathBuf,
+    line: CodeLine,
+}
+
+impl CodeReference {
+    pub fn new(path: impl Into<PathBuf>, line: impl Into<CodeLine>) -> Self {
+        Self {
+            path: path.into(),
+            line: line.into(),
+        }
+    }
 }
 
 impl Display for CodeReference {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}:{}", self.path.as_os_str().to_str().unwrap(), self.line)
+        write!(
+            f,
+            "[{}:{}]",
+            self.path.as_os_str().to_str().unwrap(),
+            self.line.0
+        )
     }
 }
 
@@ -19,19 +42,14 @@ mod tests {
 
     #[test]
     fn code_reference_has_path_and_line() {
-        let cr = CodeReference {
-            path: PathBuf::from("/foo/bar.rs"),
-            line: 32,
-        };
-        assert_eq!(32, cr.line)
+        let cr = CodeReference::new("/foo/bar.rs", 32);
+        assert_eq!(CodeLine(32), cr.line)
     }
 
     #[test]
     fn code_reference_can_be_displayed() {
-        let cr = CodeReference {
-            path: PathBuf::from("/foo/bar.rs"),
-            line: 32,
-        };
-        assert_eq!("/foo/bar.rs:32", cr.to_string())
+        let cr = CodeReference::new("/foo/bar.rs", 32);
+        assert_eq!("[/foo/bar.rs:32]", cr.to_string())
     }
 }
+
